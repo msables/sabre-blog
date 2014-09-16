2bcbdcf1bd12d083d4fea758088b9ae3c3f8cb9c
Michael Sables <msables@teneightytwo.com>
2014-09-16 05:37:21 +0100

    Add django-markdown, add blog admin

--- sabre/blog/admin.py
+++ sabre/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from  blog.models import Entry, Tag
+from django_markdown.admin import MarkDownModelAdmin
 
-# Register your models here.
+
+class EntryAdmin(MarkDownModelAdmin):
+    list_display = ("title", "created")
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Tag)
